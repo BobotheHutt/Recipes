c49d81418c031a1dff78f438773eae41d094cb04
BobotheHutt <jumprun4112@gmail.com>
2026-05-18 16:28:05 -0700

    Create config.js

---
+++ js/js/config.js
@@ -0,0 +1,10 @@
+// js/config.js
+
+const APP_CONFIG = {
+    // ⚠️ THE ONLY PLACE YOU EVER NEED TO EDIT: Paste your active Google Web App URL below
+    GOOGLE_BRIDGE_URL: "https://script.google.com/macros/s/AKfycbzbr4dDR_MNPWpwRpbQ_cbDQtB8exUmjB9r_0SznThxB8xmggLUFTtDrEcd417mqiWa/exec",
+    
+    // Your verified GitHub credentials
+    GITHUB_USERNAME: "BoobotheHutt",
+    PRIVATE_REPO_NAME: "recipes-private-backend"
+};
