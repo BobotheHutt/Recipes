594bc9c6ebe0d871547aac3a335b2b65a7946a08
BobotheHutt <jumprun4112@gmail.com>
2026-05-18 18:00:13 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 const APP_CONFIG = {
     // ⚠️ THE ONLY PLACE YOU EVER NEED TO EDIT: Paste your active Google Web App URL below
-    GOOGLE_BRIDGE_URL: "https://script.google.com/macros/s/AKfycbx0ykLI21p795e2Ul0AUkpbk74JveeFPX2qEyqLCI6YvxJY0f38h7widSiXKeKaoCHS/exec",
+    GOOGLE_BRIDGE_URL: "https://script.google.com/macros/s/AKfycbya-oV8_Ve-Yy8qoQ3tH1u6ODuw1_30VNRcqeLNc4IBdwSVBht_TTe369NaPE-wbSsJ/exec",
     
     // Your verified GitHub credentials
     GITHUB_USERNAME: "BobotheHutt",
